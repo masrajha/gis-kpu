430f5f48851aa3bc2a9033c017da418b58bdaf23
Didik Kurniawan <didikunila@gmail.com>
2019-04-10 18:09:27 +0700

    Fix class GIS

--- js/main.js
+++ js/main.js
@@ -1,27 +1,33 @@
-class Controller {
-    constructor(srcFile) {
-        this.srcFile = srcFile;
+class GIS {
+    constructor(jsonFile, map) {
+        this.jsonFile = jsonFile;
+        this.map = map;
     }
-    loadData(map, div) {
-        $.getJSON(this.srcFile, function (json) {
+    setJSONFile(url) {
+        this.jsonFile = url;
+    }
+    loadData(div = null) {
+        map = this.map;
+        $.getJSON(this.jsonFile, function(json) {
             // console.log(json); // this will show the info it in firebug console
             data = json;
             map.data.addGeoJson(json);
-            map.data.forEach(function (f) {
+            map.data.forEach(function(f) {
                 console.log(f);
             });
 
-            map.data.setStyle(function (feature) {
+            map.data.setStyle(function(feature) {
                 return /** @type {google.maps.Data.StyleOptions} */ ({
                     fillColor: feature.getProperty('color'),
                     strokeWeight: 1
                 });
             });
 
-            map.data.addListener('click', function (event) {
+            map.data.addListener('click', function(event) {
                 let content = event.feature.getProperty('description').replace(/\s+/g, " ");
                 console.log(content)
-                document.getElementById(div).textContent = content;
+                if (div)
+                    document.getElementById(div).textContent = content;
 
                 var infowindow = new google.maps.InfoWindow();
 
